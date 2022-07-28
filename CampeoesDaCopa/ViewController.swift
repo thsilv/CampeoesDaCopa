60d5e1b648c85d55646877fc11ea58821ec05ea7
thsilv <t.silvaemp@gmail.com>
2022-07-28 10:42:44 -0300

    Initial Commit

---
+++ CampeoesDaCopa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Thiago Silva on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
